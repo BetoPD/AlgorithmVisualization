f52738b2f99d5dbe69244034b61a2ba7bb2ad2fc
Humberto <humberto.angel.duran@gmail.com>
2024-02-23 08:21:56 -0600

    Add BFS and Dijkstra algorithms for pathfinding

--- bfs.py
+++ bfs.py
@@ -17,7 +17,7 @@ col, filas = ANCHO // CUADRO, ALTO // CUADRO
 
 pygame.init()
 ventana = pygame.display.set_mode(RES)
-pygame.display.set_caption("Dijkstra")
+pygame.display.set_caption("BFS")
 reloj = pygame.time.Clock()
 font = pygame.font.Font(None, 50)
 
@@ -34,8 +34,7 @@ for fila in celdas:
 # Configuracion del algoritmo
         
 celdas[0][0].inicial = True
-celdas[0][0].distancia = 0
-priority_queue = [celdas[0][0]]
+queue = [celdas[0][0]]
 
 # Ejecuta la ventana, en un while loop, por lo que no es necesario que el algoritmo este en un while dentro de este while
 
@@ -74,27 +73,20 @@ while True:
     # Cuando ya se ha indicado la celda de destino, por medio de un click derecho y todavía no se encuentre el nodo
     if start and not found:
 
-        if priority_queue:
-            # como la priority queue es de objetos, en vez de números, podemos indicarle por que atributo del objeto ordenar la lista
-            priority_queue.sort(key=lambda x: x.distancia)
-            current = priority_queue.pop(0)
+        if queue:
+            current = queue.pop(0)
             current.visited = True
 
-            # Si el nodo que buscamos es igual al que estamos, 
             if current.target:
-                path = current
                 found = True
-            
-            # iteramos en los vecinos del nodo
+                path = current
 
             for vecino in current.vecinos:
                 if not vecino.visited and not vecino.muro:
-                    distancia_nueva = current.distancia + 1
-                    if distancia_nueva < vecino.distancia:
-                        vecino.previo = current
-                        vecino.distancia = distancia_nueva
-                        priority_queue.append(vecino)
-                        vecino.frontera = True
+                    queue.insert(0, vecino)
+                    vecino.previo = current
+
+
         
         # Contador 
         seconds = (pygame.time.get_ticks() - start_ticks) // 1000
@@ -107,7 +99,7 @@ while True:
         path = path.previo
     
     # Si ya no hay nodos por visitar, y nuestra variable found es negativa, muestra el siguiente mensaje
-    if not priority_queue and not found:
+    if not queue and not found:
         text = font.render("Sin solución flaco!", True, pygame.Color("red"))
         text_rect = text.get_rect(center=(ANCHO // 2, ALTO // 2))
         ventana.blit(text, text_rect)
